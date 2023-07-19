80ead99bf63fe27edb1d97cdbcfa6f0d75f48fcb
Feddane <chafeddane@gmail.com>
2023-07-19 14:34:46 +0100

    course 1

---
+++ Kotlin fundamentals/app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinfundamentals
+
+fun main(){
+    val she = "mirna" //val can't be overwritten
+
+    var me = "chaïma" //var we can change it
+    me = "sonia"
+print("hello " + me + "!")
+}
