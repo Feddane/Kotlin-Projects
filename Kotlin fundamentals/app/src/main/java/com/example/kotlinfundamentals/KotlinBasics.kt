20cfb4ff664849c32ba21691b1d42e49a5fa57ad
Feddane <chafeddane@gmail.com>
2023-07-19 14:52:27 +0100

    course 2

--- Kotlin fundamentals/app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
+++ Kotlin fundamentals/app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
@@ -5,5 +5,17 @@ fun main(){
 
     var me = "chaïma" //var we can change it
     me = "sonia"
-print("hello " + me + "!")
+    print("hello " + me + "!")
+
+    //integer types: Bytes(8 bit), short(16 bit), long(64 bit), int(32 bit)
+    var myByte: Byte = 13
+    var myInt: Int = 13254698
+    var myLong: Long = 1323697456048
+    var myShort: Short = 132
+
+    //floating number types: Float(32bit), Double(64bit)
+    val myFloat : Float = 16.27F
+    val myDouble : Double = 16.45478764879999845
+
+
 }
