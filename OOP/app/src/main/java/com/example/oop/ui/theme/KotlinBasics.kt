653cc39d69bf15ed0f32774522decf6a00208d88
Feddane <chafeddane@gmail.com>
2023-07-22 17:31:12 +0100

    course 2

--- OOP/app/src/main/java/com/example/oop/ui/theme/KotlinBasics.kt
+++ OOP/app/src/main/java/com/example/oop/ui/theme/KotlinBasics.kt
@@ -3,12 +3,29 @@ package com.example.oop.ui.theme
 fun main(){
 
     var chaïma = person("chaima", "feddane")
-
+    //call the methode in oop
+    chaïma.myhobby()
 
 }
 
-class person (firstName:  String, lastName:  String  ){
+class person (firstName:  String, lastName:  String){
+
+    //constructor
+    constructor(firstName:  String, lastName:  String, age: Int) :this(firstName, lastName){
+            this.age = age
+        }
+
+
+    //methods
+    fun myhobby() {
+        println("my hobby is $hobby")
+    }
+
+    var hobby = "watch netflix"
+
     init {
         println("person $firstName $lastName created!!")
+
+
     }
 }
