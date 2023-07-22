dc08c8b8d8f8c7d3f4fa48e844eb537d9760b989
Feddane <chafeddane@gmail.com>
2023-07-22 17:44:05 +0100

    course 3

--- OOP/app/src/main/java/com/example/oop/ui/theme/KotlinBasics.kt
+++ OOP/app/src/main/java/com/example/oop/ui/theme/KotlinBasics.kt
@@ -1,20 +1,40 @@
 package com.example.oop.ui.theme
 
+import androidx.compose.ui.text.toLowerCase
+
 fun main(){
 
     var chaïma = person("chaima", "feddane")
     //call the methode in oop
     chaïma.myhobby()
 
+    println("the owner of chaima is ${chaïma.owner} ")
+
+
 }
 
 class person (firstName:  String, lastName:  String){
 
     //constructor
-    constructor(firstName:  String, lastName:  String, age: Int) :this(firstName, lastName){
-            this.age = age
-        }
+//    constructor(firstName:  String, lastName:  String, age: Int) :this(firstName, lastName){
+//            this.age = age
+//        }
 
+    lateinit var owner : String
+
+    var crash = "ABEL"
+
+        //getters
+
+//        get() {
+//            return field.toLowerCase()
+//        }
+
+        get() = field
+        //setters
+        set(value) {
+            field = value
+        }
 
     //methods
     fun myhobby() {
@@ -25,7 +45,7 @@ class person (firstName:  String, lastName:  String){
 
     init {
         println("person $firstName $lastName created!!")
-
+        this.owner = "mirna"
 
     }
 }
