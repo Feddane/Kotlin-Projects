a2ff703ac6a76f3f0fa43a6485effe8b22d4d410
Feddane <chafeddane@gmail.com>
2023-07-22 18:08:00 +0100

    course 5

--- OOP/app/src/main/java/com/example/oop/ui/theme/KotlinBasics.kt
+++ OOP/app/src/main/java/com/example/oop/ui/theme/KotlinBasics.kt
@@ -2,6 +2,31 @@ package com.example.oop.ui.theme
 
 import androidx.compose.ui.text.toLowerCase
 
+
+open class Car(val name: String, val brand: String) {
+    // open so it can be overriden by inhereting classes
+    open var range: Double = 0.0
+    fun extendRange(amount: Double) {
+        if(amount > 0) {
+            range += amount
+        }
+    }
+    open fun drive(distance: Double){
+        println("Drove for $distance KM")
+    }
+}
+
+class ElectricCar(name: String, brand: String, batteryLife: Double) : Car(name, brand) {
+
+    var chargerType = "Type1"
+    // in KM
+    override var range = batteryLife/6
+
+    override fun drive(distance: Double){
+        println("Drove for $distance KM on electricity")
+    }
+}
+
 fun main(){
 
     var chaïma = person("chaima", "feddane")
@@ -16,6 +41,17 @@ fun main(){
     println(user1.equals(user2))
     val user3= user1.copy(name= "maya")
     println(user3)
+
+    /************inheritance***************/
+    var audiA3 = Car("A3", "Audi")
+    var teslaS = ElectricCar("S-Model", "Tesla", 85.0)
+
+    // Polymorphism is the ability to treat objects
+    // with similar traits in a common way
+    audiA3.drive(200.0)
+    // Only works because ElectricCar is a Subclass of Car
+    // Or alternativly works if Car was a Interface and Electriccar would inherit from it
+    teslaS.drive(200.0)
 
 }
 
