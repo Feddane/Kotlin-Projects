91adc63cdda88cb472c4c26e8ddc1cfb4e5805b0
Feddane <chafeddane@gmail.com>
2023-07-22 16:14:49 +0100

    course 1

---
+++ OOP/app/src/main/java/com/example/oop/ui/theme/KotlinBasics.kt
@@ -0,0 +1,14 @@
+package com.example.oop.ui.theme
+
+fun main(){
+
+    var chaïma = person("chaima", "feddane")
+
+
+}
+
+class person (firstName:  String, lastName:  String  ){
+    init {
+        println("person $firstName $lastName created!!")
+    }
+}
