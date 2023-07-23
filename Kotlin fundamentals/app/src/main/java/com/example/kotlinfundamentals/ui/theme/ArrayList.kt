8e8ecf329db81f89b3ccd491e20547ff70f46103
Feddane <chafeddane@gmail.com>
2023-07-23 17:14:33 +0100

    coure 4 part 2

---
+++ Kotlin fundamentals/app/src/main/java/com/example/kotlinfundamentals/ui/theme/ArrayList.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinfundamentals.ui.theme
+
+fun main(){
+
+    val arraylist: ArrayList<String> = ArrayList<String>()
+    arraylist.add("one")
+    arraylist.add("two")
+    arraylist.add("three")
+
+    for (i in arraylist){
+        print(i+",")
+    }
+}
