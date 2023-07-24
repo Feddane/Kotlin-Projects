7a5d81054e1895354c2a1ba43092ba50d7beab77
Feddane <chafeddane@gmail.com>
2023-07-24 16:14:22 +0100

    design

--- Age in Minutes/app/src/main/java/com/example/dobcalc/MainActivity.kt
+++ Age in Minutes/app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -15,32 +15,7 @@ import com.example.dobcalc.ui.theme.DOBcalcTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DOBcalcTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DOBcalcTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
