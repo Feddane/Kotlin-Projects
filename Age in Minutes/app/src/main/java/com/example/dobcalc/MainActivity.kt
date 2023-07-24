94c1e328d013c523731dda2bfb359af59c433174
Feddane <chafeddane@gmail.com>
2023-07-24 17:34:25 +0100

    selected date

--- Age in Minutes/app/src/main/java/com/example/dobcalc/MainActivity.kt
+++ Age in Minutes/app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dobcalc
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,15 +14,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dobcalc.ui.theme.DOBcalcTheme
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Locale
+import java.util.SimpleTimeZone
 
 class MainActivity : ComponentActivity() {
+
+    private var tvSelectedDate : TextView? = null //nullable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
         val btn : Button = findViewById(R.id.button)
+        tvSelectedDate  = findViewById(R.id.tv)
 
         btn.setOnClickListener{
             clickDatePicker()
@@ -37,8 +44,15 @@ class MainActivity : ComponentActivity() {
         val day =  mycalendar.get(Calendar.DAY_OF_MONTH)
 
         DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            { view, selectedYear, selectedMonth, selectedDayOfMonth ->
 
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/${selectedYear}"
+
+                tvSelectedDate?.text = selectedDate
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                val theDate = sdf.parse(selectedDate)
             },
             year,
             month,
