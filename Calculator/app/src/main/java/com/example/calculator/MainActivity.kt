900819e92c1d8cbebad7357d2e80b6759781096f
Feddane <chafeddane@gmail.com>
2023-08-06 17:45:12 +0100

    onClear and onDecimalPoint

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -18,6 +18,10 @@ import com.example.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
     lateinit var tvInput: TextView
+    var lastNumeric = false
+    var lastDot = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainactivity)
@@ -28,5 +32,21 @@ class MainActivity : ComponentActivity() {
     fun onDigit (view: View){
         //to test if the buttons are running
         tvInput.append((view as Button).text)
+        lastNumeric = true
     }
+
+    fun onClear (view: View){
+        tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view:View){
+        if(lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
 }
