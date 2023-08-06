2f8a13889b62c6ed95110d0623f4de23fcca428d
Feddane <chafeddane@gmail.com>
2023-08-06 17:15:46 +0100

    add XML for the app!

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,32 +18,13 @@ import com.example.calculator.ui.theme.CalculatorTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.mainactivity)
+
+//        val btn : Button = findViewById<Button>(R.id.btnSeven)
+
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculatorTheme {
-        Greeting("Android")
+    fun onDigit (view: View){
+        Toast.makeText(this, "hello world!", Toast.LENGTH_SHORT).show()
     }
 }
