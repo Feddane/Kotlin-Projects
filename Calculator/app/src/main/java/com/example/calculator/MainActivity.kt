dd6227cbb4bb12142d47d5c2a2ed8f702dcb6931
Feddane <chafeddane@gmail.com>
2023-08-06 18:49:52 +0100

    last

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -91,7 +91,40 @@ class MainActivity : ComponentActivity() {
                         one = prefix + one
                     }
 
-                    tvInput.text = (one.toDouble() - two.toDouble()).toString()
+                    tvInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                }else if (tvValue.contains("*")){
+                    val splitvalue = tvValue.split("*")
+
+                    var one = splitvalue[0]
+                    var two = splitvalue[1]
+
+                    if (!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                }else if (tvValue.contains("+")){
+                    val splitvalue = tvValue.split("+")
+
+                    var one = splitvalue[0]
+                    var two = splitvalue[1]
+
+                    if (!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }else if (tvValue.contains("/")){
+                    val splitvalue = tvValue.split("/")
+
+                    var one = splitvalue[0]
+                    var two = splitvalue[1]
+
+                    if (!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
                 }
 
             }catch (e: ArithmeticException){
@@ -99,6 +132,15 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun removeZeroAfterDot(result: String): String{
+        var value = result
+
+        if (result.contains(".0"))
+            value = result.substring(0, result.length - 2)
+        return value
+    }
+
 
 
 }
