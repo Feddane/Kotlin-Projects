af99d2bb1c7fcd391027e3350b6310f336852624
Feddane <chafeddane@gmail.com>
2023-08-08 19:45:24 +0100

    Questions

--- Quizz/app/src/main/java/com/example/quizz/MainActivity.kt
+++ Quizz/app/src/main/java/com/example/quizz/MainActivity.kt
@@ -7,25 +7,25 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.quizz.R.id.btn_start
-//import com.example.quizz.R.id.et_name
+import com.example.quizz.R.id.et_name
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainactivity)
 
-//        val btn = findViewById<Button>(btn_start)
-//        val et_name = findViewById<EditText>(et_name)
-//
-//        btn.setOnClickListener{
-//
-//            if (et_name.text.isEmpty()){
-//                Toast.makeText(this, "Please enter your name !", Toast.LENGTH_SHORT).show()
-//            }else{
-//                val intent = Intent(this, QuizQuestionsActivity::class.java)
-//                startActivity(intent)
-//            }
-//        }
+        val btn = findViewById<Button>(btn_start)
+        val et_name = findViewById<EditText>(et_name)
+
+        btn.setOnClickListener{
+
+            if (et_name.text.isEmpty()){
+                Toast.makeText(this, "Please enter your name !", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+            }
+        }
 
 }
 }
--- Quizz/app/src/main/java/com/example/quizz/QuizQuestionsActivity.kt
+++ Quizz/app/src/main/java/com/example/quizz/QuizQuestionsActivity.kt
@@ -2,6 +2,7 @@ package com.example.quizz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import com.example.quizz.R
 
@@ -10,7 +11,7 @@ class QuizQuestionsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-
-
+        val questionsList = Constants.getQuestions()
+        Log.i("TAG","QuestionsList size is, ${questionsList.size}")
     }
 }
