a4ba72c40d2b2618316c409add93dd3ee8447987
Feddane <chafeddane@gmail.com>
2023-08-12 18:56:26 +0100

    Setting up the app Part 2

--- Drawing/app/src/main/java/com/example/drawing/DrawingView.kt
+++ Drawing/app/src/main/java/com/example/drawing/DrawingView.kt
@@ -9,24 +9,18 @@ import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 import android.graphics.*
+import android.util.Log
+import android.view.MotionEvent
 
-class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs) {
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-    private var mDrawPath : CustomPath? = null
-    private var mCanvasBitmap : Bitmap? = null
-    private var mDrawPaint : Paint? = null
-    private var mCanvasPaint : Paint? = null
-    private var mBrushSize : Float = 0.toFloat()
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
 
-    /**
-     * A variable for canvas which will be initialized later and used.
-     *
-     *The Canvas class holds the "draw" calls. To draw something, you need 4 basic components: A Bitmap to hold the pixels, a Canvas to host
-     * the draw calls (writing into the bitmap), a drawing primitive (e.g. Rect,
-     * Path, text, Bitmap), and a paint (to describe the colors and styles for the
-     * drawing)
-     */
     private var canvas: Canvas? = null
 
     init {
@@ -35,19 +29,62 @@ class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs)
 
     private fun setUpDrawing() {
         mDrawPaint = Paint()
-        mDrawPath =  CustomPath(color, mBrushSize)
+        mDrawPath = CustomPath(color, mBrushSize)
         mDrawPaint!!.color = color
         mDrawPaint!!.style = Paint.Style.STROKE
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
-        mCanvasPaint = Paint(android.graphics.Paint.DITHER_FLAG) // Paint flag that enables dithering when blitting.
+        mCanvasPaint = Paint(Paint.DITHER_FLAG) // Paint flag that enables dithering when blitting.
         mBrushSize = 20.toFloat()
-
     }
 
-    private val mPaths = ArrayList<CustomPath>() // ArrayList for Paths
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        Log.d("DrawingView", "onSizeChanged - width: $w, height: $h")
 
-    internal inner class CustomPath (var clor: Int, var brushThickness: Float) : Path(){
-
+        if (w > 0 && h > 0) {
+            mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+            canvas = Canvas(mCanvasBitmap!!)
+        }
     }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPaint!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                if (touchX != null && touchY != null) {
+                    mDrawPath!!.moveTo(touchX, touchY)
+                }
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if (touchX != null && touchY != null) {
+                    mDrawPath!!.lineTo(touchX, touchY)
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            else -> return false
+        }
+        invalidate()
+        return true
+    }
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path()
 
 }
