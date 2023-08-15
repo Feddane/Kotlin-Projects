14d2bf0d7f4190c345c8bcfc866780d90e741d07
Feddane <chafeddane@gmail.com>
2023-08-15 15:44:29 +0100

    Alert Dialog

--- Dialog/app/src/main/java/com/example/dialog/MainActivity.kt
+++ Dialog/app/src/main/java/com/example/dialog/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.dialog
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,5 +22,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val imagebutton : ImageButton =  findViewById(R.id.imageButton)
+        val button1 : Button =  findViewById(R.id.button1)
+        val button2 : Button =  findViewById(R.id.button2)
+        val button3 : Button =  findViewById(R.id.button3)
+
+
+
+        imagebutton.setOnClickListener {view ->
+            Toast.makeText(this, "imaged touched!", Toast.LENGTH_SHORT).show()
+        }
+
+        button1.setOnClickListener {view ->
+            alertDialogFunction()
+        }
+
+
+
+
+
+    }
+
+    private fun alertDialogFunction() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Alert")
+        builder.setMessage("This is Alert Dialog. Which is used to show alert")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        builder.setPositiveButton("Yes"){
+            dialogInterface, which ->
+            Toast.makeText(applicationContext, "clicked yes", Toast.LENGTH_SHORT).show()
+            dialogInterface.dismiss()
+        }
+
+        builder.setNeutralButton("cancel"){
+                dialogInterface, which ->
+            Toast.makeText(applicationContext, "clicked cancel\n operation cancel", Toast.LENGTH_SHORT).show()
+            dialogInterface.dismiss()
+        }
+        builder.setNegativeButton("No"){
+                dialogInterface, which ->
+            Toast.makeText(applicationContext, "clicked No", Toast.LENGTH_SHORT).show()
+            dialogInterface.dismiss()
+        }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false) //when i click on other area the box will not disappear
+        alertDialog.show()
     }
 }
