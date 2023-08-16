3851fdc911f2a86dad71894a5707ed6104515105
Feddane <chafeddane@gmail.com>
2023-08-16 15:48:48 +0100

    Manage Coroutines

--- Coroutines/app/src/main/java/com/example/corountines/MainActivity.kt
+++ Coroutines/app/src/main/java/com/example/corountines/MainActivity.kt
@@ -1,23 +1,66 @@
 package com.example.corountines
 
+import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.corountines.ui.theme.CorountinesTheme
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
+
+//    var customProgressDialog : Dialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnExecute : Button = findViewById(R.id.btn_execute)
+
+        btnExecute.setOnClickListener {
+//            showProgressDialog()
+           lifecycleScope.launch {
+               execute("Task executed successfully")
+           }
+        }
 
     }
+
+    private suspend fun execute(result: String) {
+        withContext(Dispatchers.IO) {
+            for (i in 1..30000) {
+                Log.e("delay", "" + i)
+            }
+            runOnUiThread{
+//                cancelProgressDialog()
+                Toast.makeText(this@MainActivity, result, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+
+    //when you want to run something but you want the user wait for a response
+//    private fun showProgressDialog() {
+//        val customProgressDialog = Dialog(this)
+//
+//        customProgressDialog.setContentView(R.layout.dialog_custom_progress)
+//
+//        customProgressDialog.show()
+//    }
+
+
+//    private fun cancelProgressDialog(){
+//        if (customProgressDialog != null){
+//            customProgressDialog?.dismiss()
+//            customProgressDialog = null
+//        }
+//    }
+
 
 }
