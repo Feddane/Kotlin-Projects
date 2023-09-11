18bb2842ce8b5183917f536a868479a7723490d4
Feddane <chafeddane@gmail.com>
2023-09-11 11:25:39 +0100

    add xml

---
+++ Movie/app/src/main/java/com/example/movieapp/simple_movie_details/SingleMovie.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp.simple_movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieapp.R
+
+class SingleMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
