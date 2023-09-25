1833d2eba7c0e48398ef2df938a8dd520848f2eb
Feddane <chafeddane@gmail.com>
2023-09-25 11:39:29 +0100

    activity_main

--- Trello Clone/app/src/main/java/com/example/trelloclone/MainActivity.kt
+++ Trello Clone/app/src/main/java/com/example/trelloclone/MainActivity.kt
@@ -15,32 +15,6 @@ import com.example.trelloclone.ui.theme.TrelloCloneTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TrelloCloneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrelloCloneTheme {
-        Greeting("Android")
     }
 }
