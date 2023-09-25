9235e9e50d8f0dfe3fd970a7b920dbdc51221cec
Feddane <chafeddane@gmail.com>
2023-09-25 12:32:06 +0100

    add a font to a textview

--- Trello Clone/app/src/main/java/com/example/trelloclone/SplashActivity.kt
+++ Trello Clone/app/src/main/java/com/example/trelloclone/SplashActivity.kt
@@ -1,11 +1,24 @@
 package com.example.trelloclone
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
+        // l'application occupe tout l'écran de l'appareil
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
+
+
     }
 }
