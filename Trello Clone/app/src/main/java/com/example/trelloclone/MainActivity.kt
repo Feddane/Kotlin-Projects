675db2c2f23138f07385fb2b2fa295b4761677c6
Feddane <chafeddane@gmail.com>
2023-09-26 11:07:58 +0100

    setContentView

--- Trello Clone/app/src/main/java/com/example/trelloclone/MainActivity.kt
+++ Trello Clone/app/src/main/java/com/example/trelloclone/MainActivity.kt
@@ -15,6 +15,6 @@ import com.example.trelloclone.ui.theme.TrelloCloneTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_splash)
     }
 }
