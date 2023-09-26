5d13070cf5c43963ee34ab28a7cd8a4821bf407b
Feddane <chafeddane@gmail.com>
2023-09-26 12:38:36 +0100

    XML for: SignUpActivity

--- Trello Clone/app/src/main/java/com/example/trelloclone/activities/IntroActivity.kt
+++ Trello Clone/app/src/main/java/com/example/trelloclone/activities/IntroActivity.kt
@@ -1,8 +1,10 @@
 package com.example.trelloclone.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
 import com.example.trelloclone.R
 
 class IntroActivity : AppCompatActivity() {
@@ -10,10 +12,16 @@ class IntroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
+       val btn_sign_up_intro = findViewById<Button>(R.id.btn_sign_up_intro)
+
         // l'application occupe tout l'écran de l'appareil
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        btn_sign_up_intro.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
