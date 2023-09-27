da9445de745e6e5eb8d38922fd192059a10a31d5
Feddane <chafeddane@gmail.com>
2023-09-27 11:51:32 +0100

    Preparing the SignUp Feature

--- Trello Clone/app/src/main/java/com/example/trelloclone/activities/SignUpActivity.kt
+++ Trello Clone/app/src/main/java/com/example/trelloclone/activities/SignUpActivity.kt
@@ -2,7 +2,11 @@ package com.example.trelloclone.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.example.trelloclone.R
 
@@ -33,6 +37,49 @@ class SignUpActivity : BaseActivity() {
         }
 
         toolbar.setNavigationOnClickListener { onBackPressed() }
+
+        val btn_sign_up = findViewById<Button>(R.id.btn_sign_up)
+        btn_sign_up.setOnClickListener {
+            registerUser()
+        }
     }
+
+
+    private fun registerUser(){
+
+        val et_name = findViewById<EditText>(R.id.et_name)
+        val et_email= findViewById<EditText>(R.id.et_email)
+        val et_password= findViewById<EditText>(R.id.et_password)
+
+        val name: String = et_name.text.toString().trim { it <= ' ' }
+        val email: String = et_email.text.toString().trim { it <= ' ' }
+        val password: String = et_password.text.toString().trim { it <= ' ' }
+
+        if (validateForm(name, email, password)){
+            Toast.makeText(this, "Now we can register a new user.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun validateForm(name: String, email: String, password: String) : Boolean{
+        return when{
+            TextUtils.isEmpty(name) ->{
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+            TextUtils.isEmpty(email) ->{
+                showErrorSnackBar("Please enter a email")
+                false
+            }
+            TextUtils.isEmpty(password) ->{
+                showErrorSnackBar("Please enter a password")
+                false
+            }else ->{
+                return true
+            }
+
+        }
+    }
+
 
 }
