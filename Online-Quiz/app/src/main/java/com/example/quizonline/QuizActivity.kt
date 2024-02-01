490ad6eb8fdf0f40e59e0d5e276b77d4b1c979a1
Feddane <chafeddane@gmail.com>
2024-02-01 14:40:49 +0100

    move to the next question

--- Online-Quiz/app/src/main/java/com/example/quizonline/QuizActivity.kt
+++ Online-Quiz/app/src/main/java/com/example/quizonline/QuizActivity.kt
@@ -3,10 +3,12 @@ package com.example.quizonline
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import com.example.quizonline.databinding.ActivityQuizBinding
 
-class QuizActivity : AppCompatActivity() {
+class QuizActivity : AppCompatActivity(), View.OnClickListener {
 
     companion object{
         var questionModelList : List<QuestionModel> = listOf()
@@ -23,6 +25,13 @@ class QuizActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityQuizBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.apply {
+            btn0.setOnClickListener(this@QuizActivity)
+            btn1.setOnClickListener(this@QuizActivity)
+            btn2.setOnClickListener(this@QuizActivity)
+            btn3.setOnClickListener(this@QuizActivity)
+            nextBtn.setOnClickListener(this@QuizActivity)
+        }
 
         loadQuestions()
         startTimer()
@@ -56,6 +65,18 @@ class QuizActivity : AppCompatActivity() {
             btn1.text = questionModelList[currentQuestionIndex].options[1]
             btn2.text = questionModelList[currentQuestionIndex].options[2]
             btn3.text = questionModelList[currentQuestionIndex].options[3]
+        }
+    }
+
+    override fun onClick(view: View?) {
+        val clickedBtn = view as Button
+
+        if (clickedBtn.id ==  R.id.next_btn){
+            // next button is clicked
+            currentQuestionIndex++
+            loadQuestions()
+        }else{
+            //options button is clicked
         }
     }
 
