ba98398472bbfb8499100c4e019432a347ccf27f
Feddane <chafeddane@gmail.com>
2024-07-30 16:49:56 +0100

    MainActivity

--- Note app/app/src/main/java/com/example/noteapp/MainActivity.kt
+++ Note app/app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.noteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.noteapp.database.NoteDatabase
+import com.example.noteapp.repository.NoteRepository
+import com.example.noteapp.viewmodel.NoteViewModel
+import com.example.noteapp.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
